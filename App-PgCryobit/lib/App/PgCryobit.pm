ed77f860bff1f473fff46a8d1c49d6c4b7d7e686
Jerome Eteve <jerome.eteve@gmail.com>
2010-10-05 21:58:38 +0100

    Skeleton of App::PgCryobit passes test

--- App-PgCryobit/lib/App/PgCryobit.pm
+++ App-PgCryobit/lib/App/PgCryobit.pm
@@ -1,51 +1,43 @@
 package App::PgCryobit;
 
-use warnings;
-use strict;
+use Moose;
+use Config::General;
 
 =head1 NAME
 
-App::PgCryobit - The great new App::PgCryobit!
+App::PgCryobit - The pg_cryobit application
 
 =head1 VERSION
 
 Version 0.01
 
+=head1 SYNOPSIS
+
+Use the pg_cryobit command to call this.
+
+=head1 SUBROUTINES/METHODS
+
 =cut
 
 our $VERSION = '0.01';
 
+has 'config_paths' => ( is => 'ro' , isa => 'ArrayRef', required =>  1);
+has 'configuration' => ( is => 'ro' , isa => 'HashRef' , lazy_build => 1 ); 
 
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use App::PgCryobit;
-
-    my $foo = App::PgCryobit->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
+sub _build_configuration{
+    my ($self) = @_;
+    my %configuration;
+    foreach my $path ( @{$self->config_paths()} ){
+	if( -f $path && -r $path ){
+	    %configuration = Config::General::ParseConfig($path);
+	    return \%configuration;
+	}
+	if( -d $path && -r $path.'/pg_cryobit.conf' ){
+	    %configuration = Config::General::ParseConfig($path.'/pg_cryobit.conf');
+	    return \%configuration;
+	}
+    }
+    die "No pg_cryobit.conf could be found in paths ".join(':',@{$self->config_paths()}); 
 }
 
 =head1 AUTHOR
@@ -57,8 +49,6 @@ Jerome Eteve, C<< <jerome at eteve.net> >>
 Please report any bugs or feature requests to C<bug-app-pgcryobit at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-PgCryobit>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
 
 
 =head1 SUPPORT
--- App-PgCryobit/t/00-load.t
+++ App-PgCryobit/t/00-load.t
@@ -1,10 +1,21 @@
 #!perl -T
 
-use Test::More tests => 1;
-
+use Test::More tests => 8;
+use Test::Exception;
 BEGIN {
     use_ok( 'App::PgCryobit' ) || print "Bail out!
 ";
 }
 
 diag( "Testing App::PgCryobit $App::PgCryobit::VERSION, Perl $], $^X" );
+
+my $cryo;
+dies_ok( sub{ $cryo = App::PgCryobit->new(); }, "Dies without config paths");
+lives_ok( sub{ $cryo = App::PgCryobit->new({ config_paths => [ '/ojfjj/' ] });}, "Lives with good config");
+dies_ok( sub{ my $conf = $cryo->configuration(); } , "Dies on building the conf");
+## Replaces it with a correct conf one.
+lives_ok( sub{ $cryo = App::PgCryobit->new({ config_paths => ['conf_test'] }); }, "Lives with good test config");
+ok( my $conf = $cryo->configuration() , "Conf is loaded");
+## Try the same thing with a file path
+lives_ok( sub{ $cryo = App::PgCryobit->new({ config_paths => ['conf_test/pg_cryobit.conf'] }); }, "Lives with good test config");
+ok( $conf = $cryo->configuration() , "Conf is loaded");
